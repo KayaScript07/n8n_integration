289093812c55904d2b946b11bbf746e94002dffd
KayaScript07 <kalymuno@gmail.com>
2025-12-09 12:36:35 +0200

    Enter the first part of the task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,49 @@
-alert("Hello, world!");
-console.log("Hi");
+const a = 10;
+const b = 20;
+const multiplicationResult = a * b;
+console.log('multiplicationResult', multiplicationResult);
+
+
+const c = 30;
+const d = 10;
+const quotient = c / d;
+console.log('quotient', quotient);
+
+const e = 15.56;
+const f = 16.32;
+const sum = e + f;
+console.log('sum', sum);
+
+console.log(typeof 11);
+console.log(typeof true);
+
+const testValue1 = 11;
+const testValue2 = true;
+const testValue3 = "java script";
+const testValue4 = "100";
+console.log('typeof testValue1:', typeof testValue1);
+console.log('typeof testValue2:', typeof testValue2);
+console.log('typeof testValue3:', typeof testValue3);
+console.log('typeof testValue4:', typeof testValue4);
+
+const num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+
+/*const userValue1 = prompt('Please enter a numeric value'); //тут краще використати const чи let?
+const squaredValue = userValue1 ** 2;
+alert(squaredValue);*/
+
+
+const userValue2 = prompt('Please enter your first numeric value');
+const userValue3 = prompt('Please enter your second numeric value');
+const averageValue = (userValue2 + userValue3) /2;
+alert(averageValue);
+
+
+
